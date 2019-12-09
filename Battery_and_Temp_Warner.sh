941e4944148b16f8e474282c0e95cfc3e4e597b0
johnwuelk <johnwuelk@yahoo.de>
2019-12-09 00:24:28 -0500

    Now it works properly. acpi and lm-sensors should be installed in advance.

--- Battery_and_Temp_Warner.sh
+++ Battery_and_Temp_Warner.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
+export LANG=en_US.UTF-8
 ###################################################################
-#Script Name	:Battery and Temp Warner
+#Script Name    :Battery and Temp Warner
 #Description	:This script checks the battery status and CPU temperature and warns the user
-#              when battery is discharging and percentage is too low and also
-#              when the CPU temperature is too high.
-#              The limits can be changed/reset in the code
-#              ($number -gt XX for battery and arrayCpuTemp[0] -gt XX for CPU temp)
+#                   when battery is discharging and percentage is too low and also
+#                   when the CPU temperature is too high.
+#                   The limits can be changed/reset in the code if desired.
+#                   ($number -gt XX for battery and arrayCpuTemp -gt XX for CPU temp)
 #Args         :
 #Author       :Johannes Bottex-Wülk
 #Email        :johnwuelk@yahoo.de
 ###################################################################
 
-battery="acpi | head -n 1"
+battery="acpi | grep -o 'Discharging.*%'"
 cpuTemp="sensors | grep -A 0 'CPU' | grep -o '[0-9]\+'"
-outputBattery=$(eval $battery)
-outputCpuTemp=$(eval $cpuTemp)
-#echo $'\n'
 
 while true #loop forever
 do
 
-  if [[ $outputBattery == *"Full"* ]] #check if plugged in
+outputBattery=$(eval $battery)
+outputCpuTemp=$(eval $cpuTemp)
+
+  if [[ $outputBattery == *"Discharging"* ]] #check if plugged in
   then
     #echo "Battery discharging."
     #write output into array with space as delimiter
@@ -30,22 +31,22 @@ do
       if [[ $element == *"%"* ]]
       then
         number=$(echo "$element" | tr -d %)
-        if [[ $number -lt 30 ]] #check if battery status under 30
+        if [[ $number -lt  30 ]] #check if battery status under 30
         then
-          batteryNotify="Please charge battery now. Draining it too low reduces its lifespan."
-          notify-send "$batteryNotify" #or notify with zenity
+          batteryNotify="Please charge battery now. \nDraining it too low reduces its lifespan."
+          zenity --no-wrap --warning --text "$batteryNotify" #or old: notify-send
         fi
       fi
     done
   fi
 
 IFS=$'\n' read -r -a arrayCpuTemp <<< "$outputCpuTemp"
-if [[ arrayCpuTemp -gt 40 ]] #check if cpu temp over 80 °C
+if [[ arrayCpuTemp -gt 80 ]] #check if cpu temp over 80 °C
 then
   cpuTempNotify="CPU has reached "
   cpuTempNotify+="$arrayCpuTemp"
-  cpuTempNotify+=" C, please cool immediately or shut down computer."
-  notify-send "$cpuTempNotify"
+  cpuTempNotify+=" °C, \nplease cool immediately \nor shut down computer."
+  zenity --no-wrap --warning --text "$cpuTempNotify"
 fi
 
 sleep 120 #check every 2 minutes
